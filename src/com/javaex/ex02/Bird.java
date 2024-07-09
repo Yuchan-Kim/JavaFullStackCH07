1aa3c14ba5949309399a22ec4ab017e508d07319
Yuchan Kim <yuchankim0906@gmail.com>
2024-07-09 16:04:50 +0900

    Final Commit

---
+++ src/com/javaex/ex02/Bird.java
@@ -0,0 +1,14 @@
+package com.javaex.ex02;
+
+public abstract class Bird {
+    protected String name;
+    
+    public abstract void sing();
+    public abstract void fly();
+    public abstract void setName(String name);
+    protected abstract String getName();
+    public abstract void showName();
+
+
+
+}
---
+++ src/com/javaex/ex02/Duck.java
@@ -0,0 +1,28 @@
+package com.javaex.ex02;
+
+public class Duck extends Bird {
+	@Override
+    public void sing() {
+		System.out.println("오리(" + this.getName() + ")가 소리내어 웁니다.");
+    }
+    @Override
+    public void fly() {
+		System.out.println("오리(" + this.getName() + ")가 날지 않습니다.");
+
+    }
+    @Override
+    public void showName() {
+    	System.out.println("오리의 이름은 " + this.getName() + " 입니다.");
+    }
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	protected String getName() {
+		return this.name;
+	}
+
+}
